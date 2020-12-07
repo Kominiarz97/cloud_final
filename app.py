9312a9262d2b64d057d69a0ecade15cf40a5c30f
Maciek Krawiec <34010885+Kominiarz97@users.noreply.github.com>
2020-12-07 22:18:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,41 +4,50 @@ conn = psycopg2.connect(
         user='mkrawiec',
         password='password',
         host='10.0.10.3',
-        port='3306',
+        port='5432',
         database='mkrawiec_db',
         )
-
+print("Połączono\n")
 cur = conn.cursor()
-create_table = 'CREATE TABLE "Uzytkownicy" (id SERIAL PRIMARY KEY, "Imie" varchar(30) NOT NULL, "Nazwisko" varchar(30) NOT NULL);'
-insert_init_values = 'INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko" VALUES ("Jan", "Kowalski"), ("Adam", "Malinowski"), ("Zbigniew", "Wodecki");'
+imie_1 = "Jan"
+nazwisko_1 = "Kowalski"
+imie_2 = "Adam"
+nazwisko_2 = "Malinowski"
+imie_3 = "Zbigniew"
+nazwisko_3 = "Wodecki"
+create_table = 'CREATE TABLE IF NOT EXISTS "Uzytkownicy" (id SERIAL PRIMARY KEY, "Imie" varchar(30) NOT NULL, "Nazwisko" varchar(30) NOT NULL);'
 select = 'SELECT * FROM "Uzytkownicy";'
 cur.execute(create_table)
 conn.commit()
-cur.execute(insert_init_values)
+print("Utworzono tabele\n")
+cur.execute('INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko") VALUES (%s, %s);', (imie_1, nazwisko_1))
 conn.commit()
-while True:
-    number = input("1 - dodaj uzytkownika\n 2 - usun uzytkownika\n 3-aktualizuj dane uzytkownika\n 4-wyswietl uzytkownikow\n 5-koniec\n")
-    if number == '1':
-        imie = input("Imie: ")
-        nazwisko = input("Nazwisko: ")
-        cur.execute('INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko" VALUES ("%s", "%s");',imie, nazwisko)
-        conn.commit()
-    elif number == '2':
-        id = input("Id uzytkownika: ")
-        cur.execute('DELETE WHERE id=%s;', id)
-        conn.commit()
-    elif number == '3':
-        id = input("Id uzytkownika: ")
-        imie = input("Nowe imie: ")
-        nazwisko = input("Nowe nazwisko: ")
-        cur.execute('UPDATE "Uzytkownicy" SET "Imie"=%s, "Nazwisko"=%s WHERE id=%s;', imie, nazwisko, id)
-        conn.commit()
-    elif number == '4':
-        cur.execute(select)
-        conn.commit()
-    elif number == '5':
-        break
-    else:
-        continue
+cur.execute('INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko") VALUES (%s, %s);', (imie_2, nazwisko_2))
+conn.commit()
+cur.execute('INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko") VALUES (%s, %s);', (imie_3, nazwisko_3))
+conn.commit()
+print("Dodano 3 rekordy do bazy\n")
+
+print("1 - dodaj uzytkownika 2 - usun uzytkownika 3-aktualizuj dane uzytkownika 4-wyswietl uzytkownikow 5-koniec")
+number = input()
+if number == '1':
+    imie = input("Imie: ")
+    nazwisko = input("Nazwisko: ")
+    cur.execute('INSERT INTO "Uzytkownicy" ("Imie", "Nazwisko") VALUES ("Imie", "Nazwisko" VALUES (%s, %s);',(imie, nazwisko))
+    conn.commit()
+elif number == '2':
+    id = input("Id uzytkownika: ")
+    cur.execute('DELETE FROM "Uzytkownicy" WHERE id=%s;', id)
+    conn.commit()
+elif number == '3':
+    id = input("Id uzytkownika: ")
+    imie = input("Nowe imie: ")
+    nazwisko = input("Nowe nazwisko: ")
+    cur.execute('UPDATE "Uzytkownicy" SET "Imie"=%s, "Nazwisko"=%s WHERE id=%s;', (imie, nazwisko, id))
+    conn.commit()
+elif number == '4':
+    cur.execute(select)
+    conn.commit()
 
 cur.close()
+conn.close()
